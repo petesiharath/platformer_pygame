14df261896a905d836cfeaca3a9b5bc5d699cea4
petesiharath <pete.siharath@gmail.com>
2024-02-12 01:22:20 +0000

    added module for making entities

---
+++ scripts/entities.py
@@ -0,0 +1,18 @@
+import pygame
+
+class PhysicsEntity:
+    def __init__(self, game, entityType, position, size):
+        self.game = game
+        self.type = entityType
+        self.position = list(position)
+        self.size = size
+        self.velocity = [0, 0]
+
+    def update(self, movement=(0, 0)):
+        frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
+
+        self.position[0] += frame_movement[0]
+        self.position[1] += frame_movement[1]
+
+    def render(self, surface):
+        surface.blit(self.game.assets["player"], self.pos)
