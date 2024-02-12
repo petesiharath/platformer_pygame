df79a51931346d758d2b4aafeda12efd0481dad1
petesiharath <pete.siharath@gmail.com>
2024-02-12 03:35:18 +0000

    physics objects stop you

--- scripts/entities.py
+++ scripts/entities.py
@@ -8,11 +8,39 @@ class PhysicsEntity:
         self.size = size
         self.velocity = [0, 0]
 
-    def update(self, movement=(0, 0)):
+    def rect(self):
+        return pygame.Rect(self.position[0], self.position[1], self.size[0], self.size[1])
+
+    def update(self, tilemap, movement=(0, 0)):
         frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
 
         self.position[0] += frame_movement[0]
+        entity_rect = self.rect()
+
+        for rect in tilemap.physics_rects_around(self.position):
+            if entity_rect.colliderect(rect):
+
+                if frame_movement[0] > 0:
+                    entity_rect.right = rect.left
+
+                if frame_movement[0] < 0:
+                    entity_rect.left = rect.right
+
+                self.position[0] = entity_rect.x
+
         self.position[1] += frame_movement[1]
+        entity_rect = self.rect()
+
+        for rect in tilemap.physics_rects_around(self.position):
+            if entity_rect.colliderect(rect):
+
+                if frame_movement[1] > 0:
+                    entity_rect.bottom = rect.top
+
+                if frame_movement[1] < 0:
+                    entity_rect.top = rect.bottom
+
+                self.position[1] = entity_rect.y
 
         self.velocity[1] = min(5, self.velocity[1] + 0.1)
 
