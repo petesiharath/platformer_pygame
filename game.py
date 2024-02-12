5bf098df2c73e4a5e55beef01c45157e0a233397
petesiharath <pete.siharath@gmail.com>
2024-02-12 01:44:02 +0000

    main .py file displays small portion of screen

--- game.py
+++ game.py
@@ -1,35 +1,33 @@
 import pygame
 import sys
 
+from scripts.entities import PhysicsEntity
+from scripts.utilities import load_image
+
 class Game:
     def __init__(self):
         pygame.init()
 
         pygame.display.set_caption("Platformer")
         self.screen = pygame.display.set_mode((640, 480))
+        self.display = pygame.Surface((320, 240))
 
         self.clock = pygame.time.Clock()
 
-        self.playerImage = pygame.image.load("data/images/player.png")
-        self.playerImagePosition = [160, 260]
-        self.playerMovement = [0, 0]
+        self.assets = {
+            "player": load_image("entities/player.png")
+        }
 
-        self.collisionArea = pygame.Rect(50, 50, 300, 50)
+        self.player = PhysicsEntity(self, "player", (50, 50), (8, 15))
+        self.playerMovement = [0, 0]
 
     def run(self):
         while True:
             
-            self.screen.fill((14, 219, 248))
+            self.display.fill((14, 219, 248))
 
-            img_r = pygame.Rect(self.playerImagePosition[0], self.playerImagePosition[1], self.playerImage.get_width(), self.playerImage.get_height())
-
-            if img_r.colliderect(self.collisionArea):
-                pygame.draw.rect(self.screen, (0, 100, 255), self.collisionArea)
-            else:
-                pygame.draw.rect(self.screen, (0, 255, 255), self.collisionArea)
-
-            self.playerImagePosition[1] += self.playerMovement[1] - self.playerMovement[0]
-            self.screen.blit(self.playerImage, self.playerImagePosition)
+            self.player.update((self.playerMovement[1] - self.playerMovement[0], 0))
+            self.player.render(self.display)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -38,19 +36,21 @@ class Game:
                     sys.exit()
                 
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_w:
+                    if event.key == pygame.K_a:
                         self.playerMovement[0] = 1
-                    if event.key == pygame.K_s:
+                    if event.key == pygame.K_d:
                         self.playerMovement[1] = 1
                 
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_w:
+                    if event.key == pygame.K_a:
                         self.playerMovement[0] = 0
-                    if event.key == pygame.K_s:
+                    if event.key == pygame.K_d:
                         self.playerMovement[1] = 0
 
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
             pygame.display.update()
             self.clock.tick(60)
+
 
 Platformer = Game()
 Platformer.run()
