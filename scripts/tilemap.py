eb2dba53fec8465f11a4876b592ecead68e2318b
petesiharath <pete.siharath@gmail.com>
2024-02-23 19:01:06 +0000

    added extract method

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -28,6 +28,28 @@ class Tilemap:
         self.offgrid_tiles = []
 
 
+    def extract(self, id_pairs, keep=False):
+
+        matches = []
+        for tile in self.offgrid_tiles.copy():
+            if (tile["type"], tile["variant"]) in id_pairs:
+                matches.append(tile.copy())
+                if not keep:
+                    self.offgrid_tiles.remove(tile)
+        
+        for location in self.tilemap:
+            tile = self.tilemap[location]
+            if (tile["type"], tile["variant"]) in id_pairs:
+                matches.append(tile.copy())
+                matches[-1]["position"] = matches[-1]["position"].copy()
+                matches[-1]["position"][0] *= self.tile_size
+                matches[-1]["position"][1] *= self.tile_size
+                if not keep:
+                    del self.tilemap[location]
+
+        return matches
+
+
     def tiles_around(self, position):
 
         tiles = []
