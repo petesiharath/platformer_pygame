091217699334fe00671433ae30b84f4bc8ec10b9
petesiharath <pete.siharath@gmail.com>
2024-02-23 03:37:14 +0000

    removed default tiles

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -13,10 +13,6 @@ class Tilemap:
         self.tilemap = {}
         self.offgrid_tiles = []
 
-        for i in range(10):
-            self.tilemap[str(3 + i) + ";10"] = {"type": "grass", "variant": 1, "position": (3 + i, 10)}
-            self.tilemap["10;" + str(5 + i)] = {"type": "stone", "variant": 1, "position": (10, 5 + i)}
-
 
     def tiles_around(self, position):
 
